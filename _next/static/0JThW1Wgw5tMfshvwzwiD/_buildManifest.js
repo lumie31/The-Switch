31f2834f8538c9505043b75ef788c83c5b563e88
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-11 10:14:55 +0000

    Deploy lumie31/The-Switch to github.com/lumie31/The-Switch.git:gh-pages

---
+++ _next/static/0JThW1Wgw5tMfshvwzwiD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6c0f3699fa5b370e2226.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f9ecbfef88af3fc86a82.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
